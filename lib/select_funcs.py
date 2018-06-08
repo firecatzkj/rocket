081c00505a1fffa4841570838862f65c5125ebe9
firecatzkj <zzkkjj@foxmail.com>
2018-06-08 17:46:07 +0800

    添加select_function

--- lib/select_funcs.py
+++ lib/select_funcs.py
@@ -2,11 +2,11 @@
 """
 User Define Feature Select Function
 """
-
 import pandas as pd
 from tqdm import tqdm
 from copy import copy
 from smbinning.smbinning import calc_iv
+from sklearn.preprocessing import *
 
 
 def drop_useless(df, *useless):
@@ -39,3 +39,39 @@ def drop_by_iv(df, y):
     selected_vars = list(result[result.IV >= 0.02]["columns"])
     selected_vars.append("fpd")
     return selected_vars
+
+
+def delMissing(df, thresold, cols=[], include=True):
+    if len(cols) == 0:
+        cols = df.columns
+    total = len(df)
+    for col in cols:
+        missing = len(df[pd.isnull(df[col])])
+        percent = round(missing / total, 2)
+        if include is True and percent >= thresold:
+            df = df.drop(col, axis=1)
+        elif include is False and percent > thresold:
+            df = df.drop(col, axis=1)
+
+
+def handleMissing(df, missingValue, cols=[], strategies=[]):
+    for i in range(len(cols)):
+        imp = Imputer(missing_values=missingValue, strategy=strategies[i])
+        df[cols[i]] = imp.fit_transform(df[cols[i]].values.reshape(-1, 1))
+
+
+# 连续变量转离散变量
+def turnDispersed(df, cols):
+    le = LabelEncoder()
+    for col in cols:
+        df[col] = le.fit_transform(df[col])
+    return df.columns
+
+
+# 离散变量转哑变量
+def turnOneHot(df, cols):
+    for col in cols:
+        fd = pd.get_dummies(df[col], prefix=col)
+        df = pd.concat([df, fd], axis=1)
+    df = df.drop(cols, axis=1)
+    return df.columns
